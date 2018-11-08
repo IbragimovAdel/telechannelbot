afb5efc576df008d0ac08ed3c0698b0f42d8e817
IbragimovAdel <ibragimovadel2001@gmail.com>
2018-11-08 14:42:08 +0300

    Fix 14:42

--- src/main/java/WeatherTT.java
+++ src/main/java/WeatherTT.java
@@ -5,14 +5,6 @@ import java.util.TimerTask;
 
 public class WeatherTT extends TimerTask {
     public void run() {
-<<<<<<< HEAD
         Bot.sendMsg("/weather");
-=======
-        try {
-            Bot.sendMsg("/weather");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
->>>>>>> 3c760c6e16a742c6ea52c27af8c623205465d598
     }
 }
