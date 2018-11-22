6a035dd73a437cd5b0a4189d93d9a6c8393b8a56
IbragimovAdel <ibragimovadel2001@gmail.com>
2018-11-22 11:20:58 +0300

    Update BotSettings.java

--- src/main/java/BotSettings.java
+++ src/main/java/BotSettings.java
@@ -3,7 +3,7 @@ public class BotSettings {
     public static final String BOT_USERNAME = "channelcontroller_topbot";
     public static final String BOT_TOKEN  = "728856600:AAEBb5U9hw2GN2y_zUHvnIsP6BBpxbD9Pjw";
 
-    public static String CHANNEL_ID = "@Hybridic";
+    public static String CHANNEL_ID = "@f1kzn";
 
     public static void setChannelId(String s){
         CHANNEL_ID = s;
